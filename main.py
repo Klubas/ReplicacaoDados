f730126164b27cf73c66b099ffa4fe7ca755306f
lucas.kluber <lucas.kluber@gmail.com>
2019-06-06 01:52:51 -0300

    conexao com banco de dados

--- main.py
+++ main.py
@@ -18,17 +18,19 @@ Cliente
 
 Referências
 https://medium.com/podiihq/networking-how-to-communicate-between-two-python-programs-abd58b97390a
+https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html
+https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html
 
 """
 
 import sys
 import socket
 
-from model.database import DataBase
+from model.database import DataBase, Tabela
 
 
 def client():
-    server_addres = get_host().split(":")
+    server_addres = get_host()
     host = server_addres[0]
 
     try:
@@ -42,20 +44,26 @@ def client():
     s.connect((host, port))
 
     message = input('-> ')
-    while message != 'q':
-        try:
-            s.send(message.encode('utf-8'))
-            data = s.recv(1024).decode('utf-8')
-            print('Received from server: ' + data)
-            message = input('==> ')
-        except KeyboardInterrupt:
-            print("Conexão Encerrada\n")
+    #while message != 'q':
+    try:
+        s.send(message.encode('utf-8'))
+        data = s.recv(1024).decode('utf-8')
+        print('Received from server: ' + data)
+        message = input('==> ')
+    except KeyboardInterrupt:
+        print("Conexão Encerrada\n")
     s.close()
 
 
 def server():
-    host = get_host()  # get local machine name
-    port = int(sys.argv[2])  # Make sure it's within the > 1024 $$ <65535 range
+
+    db = setup_db_connection("server")
+    table = Tabela('LAUDOS', 'numero_laudo', db)
+    r = table.create('numero_laudo: valor, descricao: desc')
+    print("Retorno="+str(r))
+
+    host = get_host()
+    port = int(sys.argv[2])
 
     print("Host: " + host + ":" + str(port))
 
@@ -66,35 +74,37 @@ def server():
     c, addr = s.accept()
 
     print("Connection from: " + str(addr))
-    while True:
-        data = c.recv(1024).decode('utf-8')
-        if not data:
-            break
 
-        data = data.upper()
+    #while True:
+    data = c.recv(1024).decode('utf-8')
 
-        commit_to_db(data)
+    if not data:
+        #break
+        c.close()
 
-        commit_to_client_db(c, data)
+    commit_to_db(db, data)
+
+    commit_to_client_db(db, c, data)
 
     c.close()
 
-def setup_db_connection():
-    db = DataBase("server")
-    tables = db.tabelas()
+def setup_db_connection(profile):
+    db = DataBase(profile)
+    tables = db.listar_tabelas()
     print(tables)
+    return db
 
-def commit_to_db(data):
+def commit_to_db(db, data):
     print('From online user: ' + data)
 
 
-def commit_to_client_db(c, data):
+def commit_to_client_db(db ,c, data):
     c.send(data.encode('utf-8'))
 
 
 def get_host():
     if sys.argv[1] == 'client':
-        return input("Informe o endereço IP do servidor: ")
+        return input("Informe o endereço IP do servidor: ").split(":")
     else:
         return socket.gethostbyname(socket.gethostname())
 
@@ -113,14 +123,14 @@ def help(err):
 
 if __name__ == '__main__':
     if sys.argv[1] == 'server':
-        try:
-            server()
-        except Exception:
-            help(-1)
+        #try:
+        server()
+        #except Exception:
+        #    help(-1)
     elif sys.argv[1] == 'client':
-        try:
-            client()
-        except Exception:
-            help(-2)
+        #try:
+        client()
+        #except Exception:
+        #help(-2)
     else:
         help(0)
--- model/database.py
+++ model/database.py
@@ -6,18 +6,17 @@ class DataBase():
     def __init__(self, profile="default"):
         self.session = boto3.Session(profile_name=profile)
         self.client = self.session.client('dynamodb')
-        #self.resource = boto3.resource('dynamodb')
-        #self.client = boto3.client('dynamodb')
+        self.resource = self.session.resource('dynamodb')
 
-    def tabelas(self):
+    def listar_tabelas(self):
         return self.client.list_tables()['TableNames']
 
 
 class Tabela():
     def __init__(self, nome, key, db):
         self.db = db
-        self.table = self.db.resource.Table(nome)
         self.key_name = key
+        self.table = self.db.resource.Table(nome)
 
     def query(self, arg):
         try:
@@ -25,75 +24,32 @@ class Tabela():
         except Exception:
             return -1
 
-    def scan_ne(self, arg1, arg2):
-        try:
-            return self.table.scan(FilterExpression=Attr(arg1).ne(arg2))
-        except Exception:
-            return -1
+    def create(self, data):
+        data = data.split(",")
+        for i in range(0, len(data)):
+            data[i] = data[i].split(":")
 
-    def query_data_table(self, arg):
-        tmp_user = User()
-        resposta = self.query(arg)  # Dicionario com todos os dados do banco/tabela/usuario
-        if resposta != -1:
-            if len(resposta) > 0:
-                user = resposta['Items'][0]
-                tmp_user.email = user['email']
-                tmp_user.username = user['username']
-                tmp_user.first_name = user['first_name']
-                tmp_user.last_name = user['last_name']
-                tmp_user.conta = user['conta']
-        return tmp_user
+        #try:
+        if len(self.query(data[0][1])['Items']) == 0:  # testa se o item ja existe no banco
+            response = self.table.put_item(
+                Item={
+                    data[0][0]: data[0][1],
+                    data[1][0]: data[1][1]
+                }
+            )
+            return response
+        else:
+            return -2
+       # except Exception:
+       #     return -1
 
-    def create(self, user):
-        try:
-            if len(self.query(user.email)['Items']) == 0:  # testa se o item ja existe no banco
-                response = self.table.put_item(
-                    Item={
-                        'email': user.email,
-                        'username': user.username,
-                        'first_name': user.first_name,
-                        'last_name': user.last_name,
-                        'conta': user.conta
-                    }
-                )
-                return response
-            else:
-                return -2
-        except Exception:
-            return -1
-
-    """
-    {
-        TableName: "Music",
-        Item: {
-            "Artist":"No One You Know",
-            "SongTitle":"Call Me Today",
-            "AlbumTitle":"Somewhat Famous",
-            "Year": 2015,
-            "Price": 2.14,
-            "Genre": "Country",
-            "Tags": {
-                "Composers": [
-                    "Smith",
-                    "Jones",
-                    "Davis"
-                ],
-                "LengthInSeconds": 214
-            }
-        }
-    }
-    """
-
-    def update(self, user):
+    def update(self, data):
         try:
             response = self.table.update_item(
-                Key={self.key_name: user.email},
+                Key={self.key_name: data},
                 UpdateExpression='SET username = :val1 , first_name = :val2 , last_name = :val3 , conta = :val4',
                 ExpressionAttributeValues={
-                    ':val1': user.username,
-                    ':val2': user.first_name,
-                    ':val3': user.last_name,
-                    ':val4': user.conta
+                    ':val1': data
                 }
             )
             return response
